a6093039019c2f269ab169b7cf58e6b642079ebc
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-13 23:58:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do 
+    erb :new 
+  end 
+  
+  post '/articles' do 
+  Article.new(title: params[:title], content: params[:content],)
+  
+  end
+    
+  
+  
 end
