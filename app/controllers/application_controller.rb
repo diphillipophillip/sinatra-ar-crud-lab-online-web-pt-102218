6bb73da7081774177ab84f88e134958cba7af3b6
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-14 00:44:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do 
    @article = Article.find(params[:id])
-   @article.update(params[:title], params[:content])
+   @article.update()
   end 
   
     
