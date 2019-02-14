ab2af504331d7e20e207077e6e520e38cdc091d4
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-14 00:44:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do 
    @article = Article.find(params[:id])
    @article.update(params[:article])
+   redirect :
   end 
   
     
